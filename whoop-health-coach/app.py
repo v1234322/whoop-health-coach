b2fb93625c2de6fef85323d9a7b054e076128899
v1234322 <110185458+v1234322@users.noreply.github.com>
2026-08-02 21:06:22 +0800

    Update app.py

--- whoop-health-coach/app.py
+++ whoop-health-coach/app.py
@@ -13,6 +13,20 @@ app = Flask(__name__)
 
 
 # =====================
+# DATABASE CONNECTION
+# =====================
+
+def get_db_connection():
+
+    conn = psycopg2.connect(
+        os.environ.get(
+            "DATABASE_URL"
+        )
+    )
+
+    return conn
+
+# =====================
 # DATABASE INIT
 # =====================
 
