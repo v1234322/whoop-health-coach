c1ddbea7980410e17f5dee486552175f38d5d8c1
v1234322 <110185458+v1234322@users.noreply.github.com>
2026-08-07 12:08:44 +0800

    Update app.py

--- whoop-health-coach/app.py
+++ whoop-health-coach/app.py
@@ -428,9 +428,6 @@ def refresh_access_token():
         "client_secret":
         client_secret,
 
-        "redirect_uri":
-        "https://whoop-health-coach.onrender.com/callback"
-
     }
 
     print(
