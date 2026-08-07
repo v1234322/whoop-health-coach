d9e2c0eb40f200d3084846005a8eb11c161f13ba
v1234322 <110185458+v1234322@users.noreply.github.com>
2026-08-07 22:25:53 +0800

    Update app.py

--- whoop-health-coach/app.py
+++ whoop-health-coach/app.py
@@ -442,11 +442,15 @@ def refresh_access_token():
 
     }
 
-    print(payload.keys())
-    print(payload.get("scope"))
-    print(len(payload["client_id"]))
-    print(len(payload["client_secret"]))
+    print({
+        "grant_type": payload["grant_type"],
+        "scope": payload.get("scope"),
+        "client_id_length": len(payload["client_id"]),
+        "client_secret_length": len(payload["client_secret"]),
+        "refresh_length": len(payload["refresh_token"])
+    })
 
+    
     print(
         "REDIRECT URI:",
         payload.get("redirect_uri")
