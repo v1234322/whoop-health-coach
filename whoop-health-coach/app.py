a55023b2507592094a46f25de5f4610428057844
v1234322 <110185458+v1234322@users.noreply.github.com>
2026-08-08 00:01:04 +0800

    Update app.py

--- whoop-health-coach/app.py
+++ whoop-health-coach/app.py
@@ -383,7 +383,6 @@ def refresh_access_token():
         "refresh_token": refresh_token,
         "client_id": client_id,
         "client_secret": client_secret,
-        "scope": "offline"
     }
 
     print({
