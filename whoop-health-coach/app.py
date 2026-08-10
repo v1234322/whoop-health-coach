bf2a4cbc6c97a13143dcd0d9e574c3adb563138f
v1234322 <110185458+v1234322@users.noreply.github.com>
2026-08-10 23:09:13 +0800

    Update app.py

--- whoop-health-coach/app.py
+++ whoop-health-coach/app.py
@@ -2182,48 +2182,70 @@ def generate_health_report(data):
             sleep_score_data = {}
 
 
-        # =========================
-        # STAGE
-        # =========================
+    # =========================
+    # STAGE
+    # =========================
 
-        stage = sleep_score_data.get(
-            "stage_summary",
-            {}
+    stage = sleep_score_data.get(
+        "stage_summary",
+        {}
+    )
+
+
+    if not isinstance(
+        stage,
+        dict
+    ):
+        print(
+            "INVALID STAGE DATA TYPE:",
+            type(stage)
         )
 
+        stage = {}
 
-        if not isinstance(
-            stage,
-            dict
-        ):
-            print(
-                "INVALID STAGE DATA TYPE:",
-                type(stage)
+
+    deep_sleep_duration = round(
+        (
+            stage.get(
+                "total_slow_wave_sleep_time_milli",
+                0
             )
+        ) / 3600000,
+        2
+    )
 
-            stage = {}
 
-
-        sleep_duration = round(
-            (
-                stage.get(
-                    "total_light_sleep_time_milli",
-                    0
-                )
-                +
-                stage.get(
-                    "total_slow_wave_sleep_time_milli",
-                    0
-                )
-                +
-                stage.get(
-                    "total_rem_sleep_time_milli",
-                    0
-                )
+    rem_sleep_duration = round(
+        (
+            stage.get(
+                "total_rem_sleep_time_milli",
+                0
             )
-            / 3600000,
-            2
+        ) / 3600000,
+        2
+    )
+
+
+    sleep_duration = round(
+        (
+            stage.get(
+                "total_light_sleep_time_milli",
+                0
+            )
+            +
+            stage.get(
+                "total_slow_wave_sleep_time_milli",
+                0
+            )
+            +
+            stage.get(
+                "total_rem_sleep_time_milli",
+                0
+            )
         )
+        / 3600000,
+        2
+    )
 
 
         sleep_performance = sleep_score_data.get(
